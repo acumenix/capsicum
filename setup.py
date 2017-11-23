58de9d84d58d907f0a21835fe1ec552c30b51cc2
Masa Mizutani <mizutani@cookpad.com>
2017-11-23 11:27:38 +0900

    update setup tools

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
 
 setup(
     name='capsicum',
@@ -6,8 +14,8 @@ setup(
     description='Lazy Event Risk Assessment Tool',
     author='Masayoshi Mizutani',
     author_email='mizutani@sfc.wide.ad.jp',
-    install_requires=['requests'],
+    install_requires=_requires_from_file('requirements.txt'),
     url='https://github.com/m-mizutani/capsicum',
     packages=['capsicum'],
-    test_suite='tests'
 )
+
